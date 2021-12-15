35dc3f0c35ef577e255f517ac9abeeef180955e7
Maya Nedeljkovich <maya.nedeljko@gmail.com>
2021-12-15 20:46:21 +0100

    Improve login page

--- client/components/LoginContainer.ts
+++ client/components/LoginContainer.ts
@@ -5,7 +5,7 @@ import './Button'
 @customElement('dog-login-container')
 export class LoginContainer extends LitElement {
   @state()
-  roomId: string = 'neat-duck-28'
+  roomId: string = ''
 
   @property()
   error: string = ''
@@ -28,6 +28,10 @@ export class LoginContainer extends LitElement {
         font-size: var(--font-size-medium)
       }
 
+      h1, h3 {
+        font-family: var(--heading-font-family);
+      }
+
       input {
         border: 2px solid var(--primary-color);
         background-image: none;
@@ -39,8 +43,28 @@ export class LoginContainer extends LitElement {
         margin-bottom: var(--spacer-4)
       }
 
+      .join-room, .create-room {
+        width: 40%;
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        padding: 0 var(--spacer-8);
+        align-items: center;
+        justify-content: center;
+      }
+
+
+      .login {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        margin: 0 auto;
+        width: 75%
+      }
+
       label {
-        margin-bottom: var(--spacer-4)
+        margin-bottom: var(--spacer-4);
       }
 
       .error {
@@ -66,21 +90,35 @@ export class LoginContainer extends LitElement {
   render() {
     return html`
         <div class="container">
-            <label for="room_id">
-                Enter room id
-            </label id="room_id">
-            <input type="text" @keyup="${ this.updateValue }" value="${ this.roomId }"/>
+            <p>Welcome to the</p>
+            <h1>Canine Detective Agency</h1>
+            <div class="login">
+                <div class="join-room">
+                    <p>If your friends are already waiting for you, or you want to pick up where
+                        you left off last time...</p>
+                    <label for="room_id">
+                        Enter your room id
+                    </label id="room_id">
+                    <input type="text" @keyup="${ this.updateValue }" value="${ this.roomId }"/>
 
-            ${ this.error ? html`
-                <p class="error">${ this.error }</p>
-            ` : '' }
+                    ${ this.error ? html`
+                        <p class="error">${ this.error }</p>
+                    ` : '' }
 
-            <dog-button @click="${ this.joinRoom }" .disabled="${ !this.roomId || this.isLoading }">
-                Join room
-            </dog-button>
-            <p>OR</p>
-            <dog-button @click="${ this.createRoom }" .disabled="${ this.isLoading }">Create room
-            </dog-button>
+                    <dog-button @click="${ this.joinRoom }"
+                                .disabled="${ !this.roomId || this.isLoading }">
+                        Join room
+                    </dog-button>
+                </div>
+                <h3>OR</h3>
+
+                <div class="create-room">
+                    <dog-button @click="${ this.createRoom }" .disabled="${ this.isLoading }">
+                        Create a new room
+                    </dog-button>
+                </div>
+            </div>
+
         </div>
     `
   }
