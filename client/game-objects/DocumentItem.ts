cb52d4bdf20b14d4e9b9c6b8c4fae50e5924da39
Maya Nedeljkovich <maya.nedeljko@gmail.com>
2021-12-15 20:47:40 +0100

    Improve how DocumentItem displays description

--- client/game-objects/DocumentItem.ts
+++ client/game-objects/DocumentItem.ts
@@ -1,15 +1,21 @@
 import { InteractiveItem } from './InteractiveItem'
 import { PlayableScene } from '../scenes/PlayableScene'
+import { Document } from '../../interface/game-data-interface'
 
 export class DocumentItem extends InteractiveItem {
+  private document?: Document
+
   constructor(scene: PlayableScene, x: number, y: number, id: string, image: string) {
     super(scene, x, y, id, image)
-    this.registerOnOpenListeners(() => {
-      this.scene.gameStateManager.requestDocument(id).then(observation => {
-        console.log('got observation from backend', observation)
+    this.registerOnOpenListeners(async () => {
+      if (!this.document) {
+        this.document = await this.scene.gameStateManager.requestDocument(id)
+        await this.scene.gameStateManager.updateDocument(this.id)
+      }
 
-        this.scene.gameStateManager.updateDocument(this.id)
-      })
+      if (this.overlay && this.document) {
+        this.overlay.innerText = `You have discovered "${ this.document.name }". Look in Evidence > Documents to read it.`
+      }
     })
   }
 }
